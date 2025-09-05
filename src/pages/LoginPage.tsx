32e2af2120740b371e9259fb897a2d54e705c36e
Shubham Saxena <developershubh00@gmail.com>
2025-09-05 17:25:12 +0530

    ..

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -241,7 +241,7 @@ const LoginPage: React.FC = () => {
               />
             </div>
             
-            <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-300 to-Blue-800 bg-clip-text text-transparent mb-3 leading-tight">
+            <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-300 to Blue-800 bg-clip-text text-transparent mb-3 leading-tight">
               Believers
               <br />
               Consultancy
