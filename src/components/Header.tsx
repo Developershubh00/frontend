b57de1eca33428554492d7b9ca26959826a32022
Shubham Saxena <developershubh00@gmail.com>
2025-09-05 17:39:07 +0530

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -132,10 +132,10 @@ const Header: React.FC<HeaderProps> = ({
                 />
               </div>
               <div>
-                <h1 className="font-bold text-[clamp(8px,2vw,8px)] truncate bg-gradient-to-r from-blue-500 to-blue-900 bg-clip-text text-transparent">
+                <h1 className="font-bold text-[clamp(14px,2vw,14px)] truncate bg-gradient-to-r from-blue-500 to-blue-900 bg-clip-text text-transparent">
                   Believers Consultancy
                 </h1>
-                <p className="text-[clamp(7px,1vw,7px)] truncate text-slate-500">
+                <p className="text-[clamp(10px,1vw,10px)] truncate text-slate-500">
                   Powered By Believers Destination
                 </p>
               </div>
