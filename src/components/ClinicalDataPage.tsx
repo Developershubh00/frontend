9a338a8006d63e716b62dc8be16876480eca2329
Shubham Saxena <developershubh00@gmail.com>
2025-10-01 17:13:05 +0530

    ,,

--- src/components/ClinicalDataPage.tsx
+++ src/components/ClinicalDataPage.tsx
@@ -2590,7 +2590,7 @@ const ClinicalDataPage: React.FC<ClinicalDataPageProps> = ({ onBack }) => {
                   placeholder="Search institutes, states, departments..."
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
-                  className="w-full pl-10 pr-4 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+                  className="w-full pl-8 pr-4 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
                 />
               </div>
 
