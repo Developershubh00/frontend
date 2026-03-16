68274655dc73eb45decca972a222401b7beebe81
shubhdev-bd <shubham.saxena@believersdestination.com>
2026-03-16 11:07:18 +0000

    1 image stick

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1803,7 +1803,7 @@ const MainContent: React.FC<MainContentProps> = ({
   const [bgIndex, setBgIndex] = useState(0);
   const bgImages = [
   "https://cdn.dribbble.com/userupload/47072483/file/4e241bcd05ff431236aefe3ca32de0b5.png",
-  "https://cdn.dribbble.com/userupload/47072484/file/c4f9d53a79f4fe3a59f611bd51d832d0.png",
+  // "https://cdn.dribbble.com/userupload/47072484/file/c4f9d53a79f4fe3a59f611bd51d832d0.png",
 ];
 
 useEffect(() => {
@@ -2029,8 +2029,8 @@ useEffect(() => {
       {/* <div className="bg-gradient-to-r from-blue-300 to-blue-700 px-4 lg:px-6 py-6 lg:pb-20 lg:pb-24 relative overflow-hidden rounded-2xl">
         <div className="absolute inset-0 bg-black/5"></div> */}
         <div className="relative overflow-hidden rounded-2xl px-4 lg:px-6 pb-24 lg:pb-32 min-h-[280px] lg:min-h-[460px] flex flex-col justify-end">
-          <div className="absolute inset-0 bg-cover transition-opacity duration-[1200ms]" style={{ backgroundImage: `url(${bgImages[0]})`,backgroundPosition: "center 20%", opacity: bgIndex === 0 ? 1 : 0 }} />
-          <div className="absolute inset-0 bg-cover transition-opacity duration-[1200ms]" style={{ backgroundImage: `url(${bgImages[1]})`, backgroundPosition: "center 30%", opacity: bgIndex === 1 ? 1 : 0 }} />
+          <div className="absolute inset-0 bg-cover transition-opacity duration-[1200ms]" style={{ backgroundImage: `url(${bgImages[0]})`,backgroundPosition: "center 20%", }} />
+          {/* <div className="absolute inset-0 bg-cover transition-opacity duration-[1200ms]" style={{ backgroundImage: `url(${bgImages[1]})`, backgroundPosition: "center 30%", opacity: bgIndex === 1 ? 1 : 0 }} /> */}
            <div className="absolute inset-0 bg-gradient-to-b from-black/10 via-black/20 to-black/70" />
           <div className="relative max-w-7xl mx-auto w-full">
           {/* Mobile */}
